71d968ca3519a4d87809da7200e841f895a4a8dc
quelchx <quelchx@gmail.com>
2022-01-30 20:13:21 -0500

    testing another deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ const { withContentlayer } = require("next-contentlayer");
 const path = require("path");
 
 module.exports = withContentlayer()({
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+    domains: [
+      {
+        domain: "quelchx.com",
+        defaultLocale: "en-US",
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
