2a87d1810e93ae2f38e29840e6511238746bc650
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2022-05-14 19:04:01 -0400

    changed meta data

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Head from "next/head"
-import { useRouter } from 'next/router'
-
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import SOCIALS from "../../constants/social";
 import { MetaState } from "../../@types";
 
-const Meta = ({...overrides}) => {
-  const router = useRouter()
+const Meta = ({ ...overrides }) => {
+  const router = useRouter();
 
   const meta: MetaState = {
-    title: "Eric Quelch - Developer, Writer, Creator, Mentor and Tutor",
-    description: `I've been developing websites for 4 years straight. Get in touch with me to know more.`,
+    title: "Eric Quelch - Software Developer",
+    description: `Eric Quelch - Software Developer Portfolio. Check out my portfolio here at http://quelchx.com or visit my Linkedin at ${SOCIALS.linkedin}`,
     image: "https://avatars.githubusercontent.com/u/74473426?v=4",
     type: "website",
     ...overrides,
@@ -32,12 +32,12 @@ const Meta = ({...overrides}) => {
       <meta property="og:title" content={meta.title} />
       <meta property="og:image" content={meta.image} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@wcbblez" />
+      <meta name="twitter:site" content="@ericquelch" />
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" content={meta.image} />
     </Head>
-  )
-}
+  );
+};
 
-export default Meta
+export default Meta;
